633a31b1a28e09d709148f08159b1238225e222a
GENESYSLITE\GENESYS01 <genesysliteperu@gmail.com>
2022-05-25 16:46:47 -0500

    Se cambio la configuración de gulp y se optimizarón las imagenes para la web

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,29 @@
 var gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const { watch, series } = require("gulp");
+const imagemin = require("gulp-imagemin");
 //npm install gulp --save --only=dev
 gulp.task("hello", function () {
   console.log("hello!!");
 });
+gulp.task("imagemin", () => {
+  return gulp
+    .src("src/assets/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("public/images"));
+});
 gulp.task("sass", function () {
   return gulp
-    .src("scss/**/**/*.scss")
+    .src("src/scss/**/**/*.scss")
     .pipe(sass())
     .pipe(gulp.dest("public/css"));
 });
 gulp.task("sass-skins", function () {
   return gulp
-    .src("scss/**/**/*.scss")
+    .src("src/scss/**/**/*.scss")
     .pipe(sass())
     .pipe(gulp.dest("public/css/skins/"));
 });
 gulp.task("watch", () => {
-  watch("scss/**/*.scss", series("sass"));
+  watch("src/scss/**/*.scss", series("sass"));
 });
