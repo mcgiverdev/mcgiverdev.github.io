d4804b9c7c42e6b9ffbd45ab9a4d1abe86652bf4
GENESYSLITE\GENESYS01 <genesysliteperu@gmail.com>
2022-05-25 14:08:23 -0500

    Se esta avanzando la información de la web

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 /* ================================================================= typing animation ======================================================== */
 var typed = new Typed(".typing", {
-  strings: ["", "Web Designer", "Web Developer", "Graphic Designer"],
+  strings: ["", "Diseñador Web", "Desarrollador Web", "Desarrollador Movil"],
   typeSpeed: 100,
   BackSpeed: 60,
   loop: true,
